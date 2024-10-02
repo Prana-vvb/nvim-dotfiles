9946273467c41df012a9f3e32e1f8f75b8b89a5f
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2024-10-02 20:29:58 +0530

    Create plugins/init.lua

---
+++ lua/plugins/init.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+
+    config = function()
+      require "configs.lspconfig"
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "clangd",
+          "cssls",
+          "html",
+          "pylsp",
+          "rust_analyzer",
+          "ts_ls",
+        }
+      })
+    end
+  },
+
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
