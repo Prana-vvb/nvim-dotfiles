9ef59d32a3eb43073683643f5cf0ac16ce16a040
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2024-10-03 22:32:12 +0530

    Add files via upload

---
+++ lua/chadrc.lua
@@ -0,0 +1,35 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "nightowl",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.ui = {
+    telescope = { style = "bordered" }, -- borderless / bordered
+}
+
+M.term = {
+    winopts = { number = false },
+    sizes = { sp = 0.3, vsp = 0.4, ["bo sp"] = 0.3, ["bo vsp"] = 0.4 },
+    float = {
+        relative = "editor",
+        row = 0.3,
+        col = 0.25,
+        width = 0.5,
+        height = 0.4,
+        border = "single",
+    },
+}
+
+
+return M
